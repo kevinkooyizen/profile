94a5edf9370abcc1f9a783068780471cf6041f0e
Kevin Koo <kevin@tripfez.com>
2020-05-30 15:47:07 +0800

    added ldjson

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
 
     @if (strtolower(config('app.env')) === 'production')
-    <!-- Global site tag (gtag.js) - Google Analytics -->
+        <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155997821-4"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
@@ -24,6 +24,25 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Kevin Koo Yi Zen | Full-Stack Web Developer') }}</title>
+
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Person",
+      "email": "kooyizen@outlook.com",
+      "image": "images/Kevin_Koo_Yi_Zen.jpg",
+      "jobTitle": "Full-Stack Web Developer",
+      "name": "Kevin Koo Yi Zen",
+      "gender": "male",
+      "nationality": "Malaysia",
+      "url": "https://www.kooyizen.com",
+       "sameAs" : [
+        "http://github.com/kevinkooyizen",
+        "https://www.linkedin.com/in/kevinkooyizen"
+      ]
+    }
+    </script>
+
     <meta name="description" content="Kevin Koo Yi Zen. Full-Stack Web Developer. Email: kooyizen@outlook.com. Phone: +60123658503">
 
     <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
