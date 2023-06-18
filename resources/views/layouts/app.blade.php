71cbe1a02637424853734deaeb5ac220136b5250
Kevin Koo Yi Zen <19647632+kevinkooyizen@users.noreply.github.com>
2023-06-18 19:10:12 +0800

    Add import for ionicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,10 @@
 
     <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
 
+    <!-- Ionicons -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
 </head>
 <body>
 
